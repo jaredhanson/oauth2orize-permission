ee549c1e98d684474d8f9e201df046f02f5114eb
Jared Hanson <jaredhanson@gmail.com>
2016-09-22 11:25:45 -0700

    Stub out implementation.

--- test/package.test.js
+++ test/package.test.js
@@ -6,8 +6,9 @@ var expect = require('chai').expect;
 
 describe('oauth2orize-permission', function() {
   
-  it('should export hello world', function() {
-    expect(pkg.hello).to.equal('world');
+  it('should export exchanges', function() {
+    expect(pkg.grant).to.be.an('object');
+    expect(pkg.grant.permission).to.be.a('function');
   });
   
 });
